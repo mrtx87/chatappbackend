86079f738424cd0e2d55d8963b2437deb6f49155
Patrick Beiersdörfer <patrick.beiersdoerfer@arxes-tolina.de>
2019-09-08 16:37:44 +0200

    backed

--- src/main/java/com/section9/chatapp/config/SecSecurityConfig.java
+++ src/main/java/com/section9/chatapp/config/SecSecurityConfig.java
@@ -1,6 +1,6 @@
 package com.section9.chatapp.config;
 
-/*
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,59 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-*/
 
-//@Configuration
 //@EnableWebSecurity
+//@Configuration
 public class SecSecurityConfig {} /*extends WebSecurityConfigurerAdapter {
- 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-          .withUser("user1").password(passwordEncoder().encode("user1Pass")).roles("USER")
-          .and()
-          .withUser("user2").password(passwordEncoder().encode("user2Pass")).roles("USER")
-          .and()
-          .withUser("admin").password(passwordEncoder().encode("adminPass")).roles("ADMIN");
-    }
- 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        http
-          .csrf().disable()
-          .authorizeRequests()
-          .antMatchers("/admin/**").hasRole("ADMIN")
-          .antMatchers("/anonymous*").anonymous()
-          .antMatchers("/login*").permitAll()
-          .anyRequest().authenticated()
-          .and()
-          .formLogin()
-          .loginPage("/login.html")
-          .loginProcessingUrl("/perform_login")
-          .defaultSuccessUrl("/homepage.html", true)
-          //.failureUrl("/login.html?error=true")
-          .failureHandler(authenticationFailureHandler())
-          .and()
-          .logout()
-          .logoutUrl("/perform_logout")
-          .deleteCookies("JSESSIONID")
-          .logoutSuccessHandler(logoutSuccessHandler());
-    }
-     
-    private LogoutSuccessHandler logoutSuccessHandler() {
-		// TODO Auto-generated method stub
-		return null;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().fullyAuthenticated().and().httpBasic().and()
+				.csrf().disable();
 	}
 
-	private AuthenticationFailureHandler authenticationFailureHandler() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }*/
