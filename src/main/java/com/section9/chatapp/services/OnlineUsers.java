ad55cd07bcb89b9a13e1fe54e7e4637263c3c851
SirLock <gedanke@quantentunnel.de>
2019-11-23 01:04:05 +0100

    refactored shit as fuck and redesigned removeContact -> processContactsRemoving

--- src/main/java/com/section9/chatapp/services/OnlineUsers.java
+++ src/main/java/com/section9/chatapp/services/OnlineUsers.java
@@ -8,7 +8,7 @@ import com.section9.chatapp.entities.Contact;
 import com.section9.chatapp.entities.User;
 import com.section9.chatapp.mapper.UserMapper;
 
-public class ActiveUsersCache {
+public class OnlineUsers {
 
 	Map<UUID, Contact> cache;
 
@@ -20,7 +20,7 @@ public class ActiveUsersCache {
 		this.cache = cache;
 	}
 	
-	public ActiveUsersCache() {
+	public OnlineUsers() {
 		cache = new HashMap<>();
 	}
 	
